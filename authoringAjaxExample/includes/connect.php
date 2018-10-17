436115be0ba7a66cd0bf87efd8fe85aa8940b909
Robertson <j_robertson@tss-hr420-dm19.fc.ca>
2018-10-17 11:26:24 -0400

    dev.jrr.fileLocate

---
+++ authoringAjaxExample/includes/connect.php
@@ -0,0 +1,44 @@
+<?php
+$user = "root";
+$password = "root"; // windows users leave this blank
+$host = "localhost";
+$db = "cooperInfo";
+
+$conn = mysqli_connect($host, $user, $password, $db);
+mysqli_set_charset($conn, 'utf8');
+
+if (!$conn) {
+    echo "connection didn't work...";
+    exit;
+}
+
+// get all the car data
+//$myQuery = "SELECT * FROM mainmodel";
+
+// make the query, get the result
+//$result = mysqli_query($conn, $myQuery);
+
+//$rows = array();
+
+//while($row = mysqli_fetch_assoc($result)) {
+//    $rows [] = $row;
+// }
+
+if (isset($_GET["carModel"])) { //check for a parameter
+    $car = $_GET["carModel"];
+
+    $myQuery = "SELECT * FROM mainmodel WHERE model = 'car'";
+
+    $result = mysqli_query($conn, $myQuery);
+    $rows = array();
+
+    while($row = mysqli_fetch_assoc($result))  {
+        $rows[] = $row;
+    }
+
+}
+
+// send the entire result set as a json encoded array
+echo json_encode($rows);
+
+?>
